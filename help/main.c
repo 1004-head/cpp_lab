9a66df40c1bebaecf1522c3015fafa30fce8fcd7
1004-head <woduf0628@naver.com>
2022-04-11 16:39:04 +0900

    help

---
+++ help/main.c
@@ -0,0 +1,13 @@
+#include "mat_vec.h"
+
+int main(){
+
+    int x = 10, y = 5;
+
+    printf("add : %d\n", add(x, y));
+    printf("sub : %d\n", sub(x, y));
+    printf("mul : %d\n", mul(x, y));
+    printf("div : %d\n", div(x, y));
+
+    return 0;
+}
---
+++ help/mat_vec.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_H
+# define HEADER_H
+
+#include <stdio.h>
+
+int add(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+int div(int x, int y);
+
+#endif
